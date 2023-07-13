ae1443863e42b76e5c9c8f3c7a65c752131cb820
Arkadiusz Sliwowski <arek.sliwowski37@gmail.com>
2023-07-13 16:05:26 +0200

    Initial, CSS

---
+++ src/Components/Modal.tsx
@@ -0,0 +1,23 @@
+export default function Modal(){
+
+    return(
+        <div className="modal">
+            <div className="_content">
+                <div className="_header">
+                    <h2>Change phone number</h2>
+                </div>
+                <div className="_subheader">
+                    <span>Provide new phone number</span>
+                </div>
+                <div className="_inputs-box">
+                    <input className="_area-code" type="number"></input>
+                    <input className="_number" type="number" placeholder="000-000-000"></input>
+                </div>
+                <div className="_buttons">
+                        <button className="_cancel-button">Cancel</button>
+                        <button className="_save-button">Save</button>
+                </div>
+            </div>
+        </div>
+    )
+}
